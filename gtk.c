46e8bc18ff1dcd4d8e0dec8e14cbde2f8b3e0958
SAITOU Keita <keita44.f4@gmail.com>
2019-02-26 15:10:52 +0900

    Update: ボタンを追加した

--- gtk.c
+++ gtk.c
@@ -2,12 +2,18 @@
 
 int main(int argc, char** argv)
 {
-  GtkWidget *window;
-
   gtk_init(&argc, &argv);
+
+  GtkWidget *window;
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_widget_set_size_request(window, 300, 200);
-  gtk_widget_show(window);
+
+  GtkWidget *button;
+  button = gtk_button_new_with_label("Quit");
+
+  gtk_container_add(GTK_CONTAINER(window), button);
+
+  gtk_widget_show_all(window);
   gtk_main();
 
   return 0;
